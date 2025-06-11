561cd046c0eb0b4b75198a1196d5ae966fdff665
HwangInTae82 <int0802@naver.com>
2025-06-11 09:50:58 +0900

    docs: 타이틀바

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { ThemeProvider } from 'styled-components';
 import './App.css';
-import Footer from './components/Footer';
+import TitleBar from './components/TitleBar';
 import theme from './styles/theme';
 import BasicModal from './components/modal/BasicModal';
 import NotificationList from './components/NotificationList';
+import BasicFilter from './components/filter/BasicFilter';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <NotificationList />
+      <TitleBar title="추천 운동" />
     </ThemeProvider>
   );
 }
