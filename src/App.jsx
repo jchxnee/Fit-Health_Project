70425d12ddf09d8482a2a0f80e3030d829d5c539
barc0d2 <yaho73m@gmail.com>
2025-06-12 18:38:31 +0900

    feat: Main, Calendar 완성

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import theme from './styles/theme';
 import BasicModal from './components/modal/BasicModal';
 import NotificationList from './components/NotificationList';
 import Header from './components/Header';
-import CoachList from './pages/MatchingPage/CoachList';
 import CategoryMenu from './components/CategoryMenu';
 import Footer from './components/Footer';
 import BasicFilter from './components/filter/BasicFilter';
@@ -18,14 +17,14 @@ import RecommendExercise from "./pages/RecommendExercise.jsx";
 import CoachRegister from "./pages/CoachRegister.jsx";
 import RegionSelect from "./components/RegionSelect.jsx";
 import CoachCalendar from "./components/CoachDetails/CoachCalendar.jsx";
+import NextReservation from "./pages/MatchingList/NextReservation.jsx";
+import HistoryModal from "./components/modal/HistoryModal.jsx";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-        <Header/>
-      <CoachCalendar />
-        <Footer/>
+      <HistoryModal />
     </ThemeProvider>
   );
 }
