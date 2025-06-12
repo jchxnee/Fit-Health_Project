a06b4eaefe4f2c3eadb9f70e2424af9bdb19c845
jchxnee <hyeijoo1234@naver.com>
2025-06-12 12:25:24 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,21 @@ import NotificationList from './components/NotificationList';
 import BasicFilter from './components/filter/BasicFilter';
 import CommunityPage from './pages/CommunityPage';
 import GlobalStyle from './styles/GlobalStyle';
-<<<<<<< HEAD
 import MainPage from './pages/MainPage.jsx';
 import RecommendedExerciseSection from './pages/RecommendExercise.jsx';
 import CoachDetail from './pages/MatchingPage/CoachDetail';
 import CoachMatching from './pages/MatchingPage/CoachMatching';
-=======
 import CommunityPostCreationPage from './pages/CommunityPostCreationPage';
 import CommunityPostDetailPage from './pages/CommunityPostDetailPage';
->>>>>>> 028dd3831f7f3041891ceb3e416dafe6378abe5b
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-<<<<<<< HEAD
       {/* <CoachDetail /> */}
       {/* <CoachList /> */}
-      <CoachMatching />
-=======
+      {/* <CoachMatching /> */}
       <CommunityPostDetailPage />
->>>>>>> 028dd3831f7f3041891ceb3e416dafe6378abe5b
     </ThemeProvider>
   );
 }
