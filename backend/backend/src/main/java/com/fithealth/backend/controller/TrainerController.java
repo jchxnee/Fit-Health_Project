50b8c9d1656ef9d59187189fdd26308a3201a016
jchxnee <hyeijoo1234@naver.com>
2025-06-18 11:53:26 +0900

    .

--- backend/backend/src/main/java/com/fithealth/backend/controller/TrainerController.java
+++ backend/backend/src/main/java/com/fithealth/backend/controller/TrainerController.java
@@ -1,4 +1,14 @@
 package com.fithealth.backend.controller;
 
+import com.fithealth.backend.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/trainer")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class TrainerController {
 }
