84756710dc5d77dc6a5fecb4d071e8104a580380
barc0d2 <yaho73m@gmail.com>
2025-06-19 09:43:31 +0900

    Feat: trainer 만드는중...

---
+++ backend/backend/src/main/java/com/fithealth/backend/dto/Trainer/TrainerFileDto.java
@@ -0,0 +1,8 @@
+package com.fithealth.backend.dto.Trainer;
+
+import com.fithealth.backend.entity.TrainerFile;
+import lombok.*;
+
+public class TrainerFileDto {
+
+}
---
+++ backend/backend/src/main/java/com/fithealth/backend/dto/Trainer/addTrainerDto.java
@@ -0,0 +1,16 @@
+package com.fithealth.backend.dto.Trainer;
+
+import lombok.*;
+
+public class addTrainerDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class TrainerRegister {
+
+    }
+
+}
